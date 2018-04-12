b98df36d548f21f7c877fe5ed32fa715c0daa9ee
Michael Harris <michael@Michaels-MacBook-Pro.local>
2018-04-12 14:46:20 -0600

    some content

--- script.js
+++ script.js
@@ -1,21 +1,67 @@
+var reload = false;
+
 $(document).ready(function(){
-  setTimeout(incBar.bind(0), 1000);
+  $('.loading-screen .text-out').fadeIn(1000);
+  $(document).on('blur', ()=>{reload = true});
+  $(document).on('focus', ()=>{if(reload){
+    reload = false;
+    location.reload();
+  }});
+  if($('.loading').length) load();
+  else question();
 });
 
-function incBar(){
+function load(){
+  var loadingWidths = [10, 20, 20, 50, 60, 60, 64, 64, 80, 80, 100];
+  var loadAnim = anime.timeline({
+    targets: '.loading-screen .bar',
+    duration: 300,
+    easing: 'easeOutQuad'
+  });
+  loadingWidths.forEach(function(val, index){
+    var $loadScr = $('.loading-screen');
+    const scrWidth = $loadScr.width();
+    var pxVal = (val/100)*scrWidth;
+    loadAnim.add({
+      width: pxVal,
+      delay: (index == 0) ? 2000 : 0
+    });
+  });
+  loadAnim.finished.then(clearLoadscr);
+}
+
+function clearLoadscr(){
   var $loadScr = $('.loading-screen');
-  var $bar = $loadScr.find('.bar');
-  const scrWidth = $loadScr.width();
-  const segments = 5;
-  const segmentWidth = scrWidth / segments;
-  let width = Math.random() * segmentWidth;
-  let duration = Math.random() * 400 + 200;
-  if(duration < 500) duration %= 200;
-  console.log(this);
-  if(this <= segments+2) $bar.animate({width: `+=${width}px`}, 300, 'linear', incBar.bind(this+1));
-  else {
-    setTimeout(function(){
-      $bar.animate({width: '100vw'}, 300, 'linear');
-    }, 500);
-  }
+  $loadScr.removeClass('loading');
+  $loadScr.addClass('loaded');
+
+  var clearAnim = anime({
+    targets: $loadScr.get(0),
+    duration: 600,
+    easing: 'easeInOutCubic',
+    width: '0px',
+    delay: 1000
+  }).finished.then(function (){
+    $loadScr.addClass('cleared');
+    question();
+  });
+}
+
+function question(){
+  anime({
+    targets: '.question .word',
+    opacity: 1,
+    translateY: -10,
+    easing: 'easeOutQuad',
+    duration: 1000,
+    delay: 0
+  });
+  anime({
+    targets: '.question .end',
+    opacity: 1,
+    translateY: -10,
+    easing: 'easeOutQuad',
+    duration: 1000,
+    delay: 1000
+  });
 }
